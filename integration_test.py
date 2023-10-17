e61738253d67475e9446927afecc23d337866012
Vishwajeet <kumarvishwajeet004@gmail.com>
2023-10-17 07:18:20 +0700

    Update integration_test.py
    Update the test case

--- integration_test.py
+++ integration_test.py
@@ -24,16 +24,15 @@ class TestIntegration(unittest.TestCase):
 
         # run the spark job
         test_df = run_spark_job(source_path, database, table, self.spark)
-        test_df.show()
-        test_df.printSchema()
-        test_df.count()
+        test_longest_streak = test_df.select(col("longest_streak")).filter((col("customer_id") == "0023938"))
 
         self.assertEqual(len(test_df.columns), 3)
-        self.assertEqual(test_df.columns[0], "custId")
-        self.assertEqual(test_df.columns[1], "favoriteProduct")
+        self.assertEqual(test_df.columns[0], "customer_id")
+        self.assertEqual(test_df.columns[1], "favourite_product")
         self.assertEqual(test_df.columns[2], "longest_streak")
         self.assertEqual(test_df.dtypes[0][1], "int")
         self.assertEqual(test_df.count(), 885)
+        self.assertEqual(test_longest_streak.collect()[0][0], 2)
 
 
 if __name__ == '__main__':
